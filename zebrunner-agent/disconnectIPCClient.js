7d64c5d1aa8cdc8c86860bd915387774dcb592a9
alexkirillovtech <alexandr.kirillov@ext.myfitnesspal.com>
2023-01-19 20:21:15 +0100

    fix text

--- zebrunner-agent/disconnectIPCClient.js
+++ zebrunner-agent/disconnectIPCClient.js
@@ -1,8 +1,8 @@
 const ipc = require('node-ipc').default;
 
 const disconnectIPCClient = () => {
-    ipc.log('disconnect from reportportal');
-    ipc.disconnect('reportportal');
+    ipc.log('disconnect from zebrunneragent');
+    ipc.disconnect('zebrunneragent');
 };
 
 module.exports = disconnectIPCClient
